b780db9125b78c462226d4995f44725deddcd9f7
powerdoll1987 <yiran.zhou@outlook.com>
2016-08-23 16:42:47 +0800

    complete 1st edition
    CFTC long/short Z-score stat
    Next: net pos / pos change

--- COT stat.py
+++ COT stat.py
@@ -11,7 +11,9 @@ import sys
 sys.path.append('..')
 import taifook.taifook as tf
 
-
+# 计算不同投机头寸对应的盈亏
+# 输入：CFTC的投机性多头、空头和总头寸，对应品种每日价格的OCLH
+# 输出：投机头寸Z-score的分布，和不同分布对应的盈亏
 # main process
 if __name__ == '__main__':
     # 读入数据
@@ -47,17 +49,18 @@ if __name__ == '__main__':
     pos = tf.rolling(pos, -25, funcList, colNames, newColNames)
     pos.dropna(inplace = True)
     
-    # 把pos和price连接
+    # 把pos和price连接, 按周五的公布日期连接
     pos.index = pos.index.shift(3, 'D') # pos的日期是周二，要改成release date周五
     selDatePrice.index = selDatePrice.index.shift(-3, 'D') # price的日期是下周一，也改成周五
-    subPos = pos.ix[:, 0:2].copy()
-    subPos[['LONG Z-SCORE', 'SHORT Z-SCORE']] = pos[['LONG Z-SCORE', 'SHORT Z-SCORE']]
+    subPos = pos.ix[:, 0:2].copy() #原始CFTC投机头寸数据
+    subPos[['LONG Z-SCORE', 'SHORT Z-SCORE']] = pos[['LONG Z-SCORE', 'SHORT Z-SCORE']].copy()
     subSelPrice = selDatePrice[['NEXT_5D_RETURN', 'NEXT_5D_LOW', 'NEXT_5D_HIGH']].copy()
     result = pd.concat([subPos, subSelPrice], axis = 1, join = 'inner')
     
-    # 按Z-score分组
-    result['LONG ZS KEY'] = tf.histoCut(result['LONG Z-SCORE'], 0.5) #产生分组用的key
-    result['SHORT ZS KEY'] = tf.histoCut(result['SHORT Z-SCORE'], 0.5)
+    # 按Z-score分组，生成每个分组的统计数据（2维矩阵）
+    step = 0.5
+    result['LONG ZS KEY'] = tf.histoCut(result['LONG Z-SCORE'], step) #产生分组用的key
+    result['SHORT ZS KEY'] = tf.histoCut(result['SHORT Z-SCORE'], step)
     matCount = result.groupby(['LONG ZS KEY', 'SHORT ZS KEY'])['NEXT_5D_RETURN'].count().unstack()
     matAveRet = result.groupby(['LONG ZS KEY', 'SHORT ZS KEY'])['NEXT_5D_RETURN'].mean().unstack()
     matWinProb = result.groupby(['LONG ZS KEY', 'SHORT ZS KEY'])['NEXT_5D_RETURN'].apply(tf.posPct).unstack()
@@ -68,11 +71,22 @@ if __name__ == '__main__':
     matWinProb = tf.histoSort(matWinProb)
     matWinProb = tf.histoSort(matWinProb.T)
     
+    # 找到以前同一个Z-SCORE分组的数据
+    long_zs_key = tf.histoCutKey(pos.ix[-1, 'LONG Z-SCORE'], step)
+    short_zs_key= tf.histoCutKey(pos.ix[-1, 'SHORT Z-SCORE'], step)
+    histRef = pd.DataFrame()
+    i = 0    
+    while i < len(result.index):
+        if result['LONG ZS KEY'][i] == long_zs_key and result['SHORT ZS KEY'][i] == short_zs_key:
+            histRef = histRef.append(result.ix[i])
+        i += 1
+        
     # 输出
     writer = pd.ExcelWriter('output.xlsx')
     matCount.to_excel(writer, 'matCount')
     matAveRet.to_excel(writer, 'matAveRet')
     matWinProb.to_excel(writer, 'matWinProb')
+    histRef.to_excel(writer, 'historyRef')
     writer.save()
 
     a = 1
